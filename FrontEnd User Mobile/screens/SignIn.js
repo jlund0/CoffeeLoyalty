a87244b50c6e2ad957045ba2139615e3ec485112
jlund0 <julianlund22@hotmail.com>
2023-10-26 23:07:37 +0800

    adding styling user UI

--- FrontEnd User Mobile/screens/SignIn.js
+++ FrontEnd User Mobile/screens/SignIn.js
@@ -6,6 +6,8 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
+  SafeAreaView,
+  Image,
 } from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import {
@@ -17,6 +19,7 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { app } from "../firebase";
+import { FacebookSVG, TwitterSVG, GoogleSVG } from "../assets/socialSVG.js";
 
 const auth = getAuth(app);
 auth.languageCode = "it";
@@ -77,7 +80,7 @@ const HandleTwitterLogin = () => {
     .catch((error) => alert(error.message));
 };
 
-function SocialButton({ name, onPress }) {
+function SocialButton({ name, onPress, colors, SVG }) {
   return (
     <TouchableOpacity
       onPress={() => {
@@ -85,13 +88,14 @@ function SocialButton({ name, onPress }) {
       }}
       style={{
         borderColor: "#ddd",
+
         borderWidth: 2,
         borderRadius: 10,
         paddingHorizontal: 30,
         paddingVertical: 10,
       }}
     >
-      <FontAwesome name={name} height={24} width={24} />
+      <SVG height={24} width={24} />
     </TouchableOpacity>
   );
 }
@@ -113,65 +117,134 @@ export default function SignInScreen({ navigation }) {
   // });
 
   return (
-    <View>
-      <FontAwesome name="at" size={20} />
-      <TextInput
-        placeholder={"Email ID"}
-        value={email}
-        onChangeText={setEmail}
-        inputMode="email"
-      />
-      <FontAwesome name="lock" size={20} />
-      <TextInput
-        placeholder={"Password"}
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button
-        title="Login"
-        onPress={() => handleEmailandPasswordLogin(email, password)}
-      />
-      <Text>Forgot your password?</Text>
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", backgroundColor: "#FAF0E6" }}
+    >
+      <View style={{ padding: 25 }}>
+        <View style={{ alignItems: "center", marginBottom: 30 }}>
+          {/*Animated card login image*/}
+          <Image
+            source={require("../assets/Loyalty-Cards19.png")}
+            style={{ height: 300, width: 300 }}
+          ></Image>
+        </View>
+        <Text
+          style={{
+            fontFamily: "Roboto-Medium",
+            fontSize: 28,
+            fontWeight: "500",
+            color: "#333",
+            marginBottom: 30,
+          }}
+        >
+          Login
+        </Text>
 
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          marginBottom: 30,
-        }}
-      >
-        <Text style={{ textAlign: "center", color: "#666", marginBottom: 30 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <FontAwesome name="at" size={20} />
+          <TextInput
+            placeholder={" Email"}
+            value={email}
+            onChangeText={setEmail}
+            inputMode="email"
+            style={{ flex: 1, paddingVertical: 0 }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <FontAwesome name="lock" size={20} />
+          <TextInput
+            placeholder={" Password"}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            style={{ flex: 1, paddingVertical: 0 }}
+          />
+          <TouchableOpacity>Forgot your password?</TouchableOpacity>
+        </View>
+        <Button
+          title="Login"
+          onPress={() => handleEmailandPasswordLogin(email, password)}
+        />
+
+        <Text
+          style={{
+            textAlign: "center",
+            color: "#666",
+            marginBottom: 30,
+            marginTop: 30,
+          }}
+        >
           Or, login with ...
         </Text>
-        <SocialButton
-          name="facebook-square"
-          onPress={() => HandleFacebookLogin()}
-        />
-        <SocialButton name="google" onPress={HandleGmailLogin} />
-        <SocialButton name="twitter" onPress={() => HandleTwitterLogin()} />
-      </View>
 
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          marginBottom: 30,
-        }}
-      >
-        <Text>New to the app?</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-          <Text style={{ color: "#AD40AF", fontWeight: "700" }}> Register</Text>
-        </TouchableOpacity>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            marginBottom: 30,
+          }}
+        >
+          <SocialButton
+            name="facebook"
+            onPress={() => HandleFacebookLogin()}
+            colors="#1877F2"
+            SVG={FacebookSVG}
+          />
+          <SocialButton
+            name="google"
+            onPress={HandleGmailLogin}
+            colors="#4285F4,#DB4437,#F4B400,#0F9D58"
+            SVG={GoogleSVG}
+          />
+          <SocialButton
+            name="twitter"
+            onPress={() => HandleTwitterLogin()}
+            colors="#1DA1F2"
+            SVG={TwitterSVG}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginBottom: 30,
+          }}
+        >
+          <Text>New to the app?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+            <Text style={{ color: "#AD40AF", fontWeight: "700" }}>
+              {" "}
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   input: {
-    height: 40,
+    height: 50,
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    borderRadius: 20,
   },
 });
--- FrontEnd User Mobile/screens/SignUp.js
+++ FrontEnd User Mobile/screens/SignUp.js
@@ -4,27 +4,102 @@ import {
   TextInput,
   Button,
   Text,
-  StyleSheet,
   TouchableOpacity,
+  StyleSheet,
+  SafeAreaView,
+  Image,
 } from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import {
   createUserWithEmailAndPassword,
   getAuth,
   updateProfile,
+  TwitterAuthProvider,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
 } from "firebase/auth";
+
 import { app } from "../firebase";
 import { NewUser } from "../components/database";
+import { FacebookSVG, TwitterSVG, GoogleSVG } from "../assets/socialSVG";
 
 const auth = getAuth(app);
+auth.languageCode = "it";
+const GoogleProvider = new GoogleAuthProvider();
+const TwitterProvider = new TwitterAuthProvider();
+const FacebookProvider = new FacebookAuthProvider();
 
-export default function SignUpScreen() {
+const HandleGmailLogin = () => {
+  console.log("Login in Gmail");
+  signInWithPopup(auth, GoogleProvider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log("Logged in with:", user.email, user, token);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};
+const HandleFacebookLogin = () => {
+  console.log("Loggin in Facebook");
+  auth
+    .signInWithPopup(FacebookProvider)
+    .then((userCredentials) => {
+      const user = userCredentials.user;
+      console.log("Logged in with:", user.email);
+    })
+    .catch((error) => alert(error.message));
+};
+const HandleTwitterLogin = () => {
+  console.log("Login in Twitter");
+  auth
+    .signInWithPopup(TwitterProvider)
+    .then((userCredentials) => {
+      const user = userCredentials.user;
+      console.log("Logged in with:", user.email);
+    })
+    .catch((error) => alert(error.message));
+};
+
+function SocialButton({ name, onPress, colors, SVG }) {
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        onPress;
+      }}
+      style={{
+        borderColor: "#ddd",
+
+        borderWidth: 2,
+        borderRadius: 10,
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+      }}
+    >
+      <SVG height={24} width={24} />
+    </TouchableOpacity>
+  );
+}
+
+export default function SignUpScreen({ navigation }) {
   const [password, setPassword] = React.useState("");
   const [name, setName] = React.useState("");
   const [lastname, setLastname] = React.useState("");
   const [email, setEmail] = React.useState("");
   const [passwordCheck, setPasswordCheck] = React.useState("");
   async function addUser() {
+    console.log("adding user to Database");
     const response = await fetch("http://localhost:8080/users", {
       method: "POST",
       headers: {
@@ -59,79 +134,148 @@ export default function SignUpScreen() {
   };
 
   return (
-    <View>
-      <Text>Sign Up with</Text>
-      <TouchableOpacity
-        onPress={() => {}}
-        style={{
-          borderColor: "#ddd",
-          borderWidth: 2,
-          borderRadius: 10,
-          paddingHorizontal: 30,
-          paddingVertical: 10,
-        }}
-      >
-        <FontAwesome name="google" height={24} width={24} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {}}
-        style={{
-          borderColor: "#ddd",
-          borderWidth: 2,
-          borderRadius: 10,
-          paddingHorizontal: 30,
-          paddingVertical: 10,
-        }}
-      >
-        <FontAwesome name="facebook-square" height={24} width={24} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => {}}
-        style={{
-          borderColor: "#ddd",
-          borderWidth: 2,
-          borderRadius: 10,
-          paddingHorizontal: 30,
-          paddingVertical: 10,
-        }}
-      >
-        <FontAwesome name="twitter" height={24} width={24} />
-      </TouchableOpacity>
-      <Text>Or Create account with Email</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Name"
-        onChangeText={setName}
-      ></TextInput>
-      <TextInput
-        style={styles.input}
-        onChangeText={setEmail}
-        placeholder="Email"
-      ></TextInput>
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry
-        onChangeText={setPassword}
-      ></TextInput>
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm Password"
-        secureTextEntry
-        onChangeText={setPasswordCheck}
-      ></TextInput>
-      <Button
-        title="Create Account"
-        onPress={() => handleEmailSignUp(email, password, name)}
-      />
-      {/*Confirmation Pop-Up
-        <View>
-          <Text>Confirm Details</Text>
-          <Text>Name: {name}</Text>
-          <Text>Email: {email}</Text>
-          <Button>Create Account</Button>
-        </View>*/}
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, justifyContent: "center", backgroundColor: "#FAF0E6" }}
+    >
+      <View style={{ padding: 25 }}>
+        <View style={{ alignItems: "center", marginBottom: 30 }}>
+          {/*Animated card login image*/}
+          <Image
+            source={require("../assets/Loyalty-Cards19.png")}
+            style={{ height: 300, width: 300 }}
+          ></Image>
+        </View>
+        <Text
+          style={{
+            fontFamily: "Roboto-Medium",
+            fontSize: 28,
+            fontWeight: "500",
+            color: "#333",
+            marginBottom: 30,
+          }}
+        >
+          Sign Up
+        </Text>
+        <View
+          style={{
+            flexDirection: "row",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <TextInput
+            placeholder={" Name"}
+            value={name}
+            onChangeText={setName}
+            style={{ flex: 1, paddingVertical: 0 }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <FontAwesome name="at" size={20} />
+          <TextInput
+            placeholder={" Email"}
+            value={email}
+            onChangeText={setEmail}
+            inputMode="email"
+            style={{ flex: 1, paddingVertical: 0 }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            borderBottomColor: "#ccc",
+            borderBottomWidth: 1,
+            paddingBottom: 8,
+            marginBottom: 25,
+          }}
+        >
+          <FontAwesome name="lock" size={20} />
+          <TextInput
+            placeholder={" Password"}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            style={{ flex: 1, paddingVertical: 0 }}
+          />
+          {/* <TextInput
+          id="passwordCheck"
+            placeholder={" Confirm Password"}
+            value={password}
+            onChangeText={setPasswordCheck}
+            secureTextEntry
+            style={{ flex: 1, paddingVertical: 0 }}
+          /> */}
+        </View>
+        <Button
+          title="Create Account"
+          onPress={() => handleEmailSignUp(email, password, name)}
+        />
+
+        <Text
+          style={{
+            textAlign: "center",
+            color: "#666",
+            marginBottom: 30,
+            marginTop: 30,
+          }}
+        >
+          Or, Create an account with ...
+        </Text>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            marginBottom: 30,
+          }}
+        >
+          <SocialButton
+            name="facebook"
+            onPress={() => HandleFacebookLogin()}
+            colors="#1877F2"
+            SVG={FacebookSVG}
+          />
+          <SocialButton
+            name="google"
+            onPress={HandleGmailLogin}
+            colors="#4285F4,#DB4437,#F4B400,#0F9D58"
+            SVG={GoogleSVG}
+          />
+          <SocialButton
+            name="twitter"
+            onPress={() => HandleTwitterLogin()}
+            colors="#1DA1F2"
+            SVG={TwitterSVG}
+          />
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginBottom: 30,
+          }}
+        >
+          <Text>already have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text style={{ color: "#AD40AF", fontWeight: "700" }}>
+              {" "}
+              Sign in
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
