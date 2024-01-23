885c16ebbb666e5ce203a936fab6c29d66a61b56
jlund <julianlund22@hotmail.com>
2024-01-23 17:21:48 +0800

    adding to website

--- website/app/page.js
+++ website/app/page.js
@@ -1,8 +1,22 @@
 import Image from 'next/image'
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+         <header>
+      <nav>
+        <ul><a href="#"><img/></a></ul>
+        <ul><a href="#">About</a></ul>
+        <ul><a href="#">Pricing</a></ul>
+        <ul><a href="#">Contact us</a></ul>
+        <ul><a href="#">Go to console</a></ul>
+      </nav>
+    </header>
+      <body>
+
+      </body>
     </main>
   )
 }
+
